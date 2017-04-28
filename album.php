7864f381b7a6f3cfe2842f399611196575e8fe0d
FrancescoBonso <francesco.bns@live.it>
2017-04-28 18:29:34 +0200

    0.0.7
    modifiche minime a spotify.html, cambio formato ad artisti,brani,album
    in .php

---
+++ album.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Album</title>
+  </head>
+
+  <body>
+
+    <img src="">
+
+    <div class="">
+      Titolo <br>
+      Autore <br>
+      anno
+    </div>
+
+
+
+
+
+  <div>
+    <div id="immaginealbum"> 
+      <img src="albumimage.png" id="imgalbumartist" alt="album_image" style="width:60px;height:60px;"">  
+    </div>
+
+    <DIV>
+      <h5>ALBUM</h5>
+      <h3></h3>
+      <h6>Artista</h6>
+    </DIV>
+  </div>
+
+  
+  </body>
+</html>
